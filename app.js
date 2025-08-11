73d04462d0d8307c859beef53c7a4b1dc078711d
sc-designs <152136634+Sc-Designs@users.noreply.github.com>
2025-08-12 01:04:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,5 @@ app.use(cors());
 app.use("/", adminRouter)
 
 
-app.listen(process.env.PORT || 3000,()=>{
-    console.log("👷🏽‍♂️ Admin Micro Service running on 3000")
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
